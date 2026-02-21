8ece19b7784d96ce0eab45becf8c577297b1d683
Aprajita <mishraaprajita43@gmail.com>
2026-02-21 22:30:21 +0530

    10/2

---
+++ WORK/DAY-1.py
@@ -0,0 +1,19 @@
+# for i in range(10):
+#     print(i)
+
+
+
+
+x = 5
+n = 5
+
+print(n)
+
+for i in range(n):
+    print(i)
+
+    n = 5
+    print(n)
+
+    for j in range(0, n):
+        print(j)
